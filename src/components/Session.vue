5db4abb8e3c6c78b70e3ee16c2e5105d8ee281ee
Olof Bjarnason <olof.bjarnason@proagile.se>
2021-08-11 12:12:00 +0200

    MVP CSS for session

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="page" v-on:click="nextState">
-    <h1>TDD session active</h1>
+    <h1>In session</h1>
+    <p class="STATE BLUE" v-bind:class="{ ACTIVE: state==='BLUE' }">
+      Add a test
+    </p>
+    <p class="STATE RED" v-bind:class="{ ACTIVE: state==='RED' }">
+      Make it pass
+    </p>
+    <p class="STATE GREEN" v-bind:class="{ ACTIVE: state==='GREEN' }">
+      Refactor
+    </p>
     <p>Current state: {{ state }}</p>
-    <button class="button" v-on:click="$emit('end-session')">End session</button>
+    <button :disabled="state !== 'BLUE'" class="button" v-on:click="$emit('end-session')">End session</button>
   </div>
 </template>
 
@@ -30,4 +39,27 @@ export default {
 <style scoped>
 @import './button.css';
 @import './page.css';
+.BLUE {
+  background-color: lightblue;
+  border-width: 2px;
+}
+.RED {
+  background-color: lightcoral;
+}
+.GREEN {
+  background-color: lightgreen;
+}
+.ACTIVE {
+  font-size: xx-large;
+  height: 18vh;
+}
+.STATE {
+  line-height: 10vh;
+  width: 80%;
+  margin: 0 auto;
+  alignment: center;
+  text-align: center;
+  vertical-align: center;
+  height: 10vh;
+}
 </style>
