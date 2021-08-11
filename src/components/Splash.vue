2e83941e63235285d02670893e77811393c745f8
Olof Bjarnason <olof.bjarnason@proagile.se>
2021-08-11 17:39:16 +0200

    Remove starttime debug code

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -2,7 +2,6 @@
   <div class="page">
     <div class="section">
       <h1>TDD Stats app</h1>
-      <p>Starttime: {{ Date.now() }}</p>
       <p>
         Keep track of what TDD state you are in, and see how much time you spend
         in each!
