ae18d9189103f8e4b60954c390ca375a67578e27
Olof Bjarnason <olof.bjarnason@proagile.se>
2021-08-11 11:36:44 +0200

    Switch state in Session

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="session_background">
+  <div class="session_background" v-on:click="nextState">
     <h1>TDD session active</h1>
-    <p>Current state: BLUE</p>
+    <p>Current state: {{ state }}</p>
     <button class="button" v-on:click="$emit('end-session')">End session</button>
   </div>
 </template>
 
 <script>
+
+const STATES = ['BLUE', 'RED', 'GREEN']
+
 export default {
   name: "Session",
+  data: function() {
+    return {
+      state: STATES[0]
+    }
+  },
+  methods: {
+    nextState: function() {
+      var currState = STATES.indexOf(this.state)
+      var nextState = (currState + 1) % STATES.length
+      this.state = STATES[nextState]
+    }
+  }
 }
 </script>
 
