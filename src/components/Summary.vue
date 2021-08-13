88586dc5acd486cc77b091a688c0f761530cbd3e
Olof Bjarnason <olof.bjarnason@proagile.se>
2021-08-13 11:25:13 +0200

    Summary follows other pages text styling

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="page">
     <div class="section">
-      <h1>Session summary</h1>
-      <p>Total time: {{ summary.totalTime }} s</p>
-      <p>Adding tests (BLUE): {{ summary.percentBlue }}% of time ({{ summary.timeInBlue }} s)</p>
-      <p>Making them pass (RED): {{ summary.percentRed }}% of time ({{ summary.timeInRed }} s)</p>
-      <p>Refactoring (GREEN): {{ summary.percentGreen }}% of time ({{ summary.timeInGreen }} s)</p>
+      <h1 class="page_title">Session summary</h1>
+      <p class="infotext">Total time: {{ summary.totalTime }} s</p>
+      <p class="infotext">Adding tests (BLUE): {{ summary.percentBlue }}% of time ({{ summary.timeInBlue }} s)</p>
+      <p class="infotext">Making them pass (RED): {{ summary.percentRed }}% of time ({{ summary.timeInRed }} s)</p>
+      <p class="infotext">Refactoring (GREEN): {{ summary.percentGreen }}% of time ({{ summary.timeInGreen }} s)</p>
     </div>
     <button class="button" v-on:click="$emit('exit-session')">
       Exit session
