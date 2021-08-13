95008d01dd06de95a1c67726c08c8452b875af76
Olof Bjarnason <olof.bjarnason@proagile.se>
2021-08-13 11:32:20 +0200

    Add explainers to splash

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -3,9 +3,21 @@
     <div class="section">
       <h1 class="page_title">TDD Stats app</h1>
       <p class="infotext">
-        Keep track of what TDD state you are in, and see how much time you spend
-        in each!
+        Keep track of what TDD state you are in!
       </p>
+      <p class="infotext">The TDD mindsets</p>
+      <ul>
+        <li>
+          🦊 In blue state, the fox says "You are not done yet, add another test!"
+        </li>
+        <li>
+          🐇 In red state, the rabbit says "Quickly, make it pass!"
+        </li>
+        <li>
+          🐈 In green state, the cat says "Enjoy life - refactor!"
+        </li>
+
+      </ul>
     </div>
     <button class="button" v-on:click="$emit('start-session')">
       Start session
@@ -18,3 +30,9 @@ export default {
   name: "Splash"
 };
 </script>
+
+<style>
+li {
+  font-size: x-large;
+}
+</style>
